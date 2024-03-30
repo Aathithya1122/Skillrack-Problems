af1d81db5de43bddf44a5f8aefa00aec400c9872
Aathithya R <127286270+Aathithya1122@users.noreply.github.com>
2024-03-30 10:26:34 +0530

    ProgramID-10817

---
+++ DAILY-CHALLENGE/30-03-2024 - Celsius to Fahrenheit/ProgramID-10817.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double n=sc.nextDouble();
+		System.out.printf("%.2f",(double)((n*9/5) + 32));
+	}
+}
